3687c3649fc759f5753c7c96e28aecf3a9b36f5f
AlexanderPezarro <alexanderpezarro@gmail.com>
2023-03-04 17:10:59 +0000

    Add socket to front end

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Menu } from "./components/Menu";
+import { io, Socket } from "socket.io-client";
+
 
 function App() {
     const mode = "home";
+    const socket = io("http://localhost:22435");
     return (
         <div className="App">
             <header className="App-header">
