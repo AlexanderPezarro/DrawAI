bb2d37d8df04add78fecc8bc5c4eb37a6f15627f
AlexanderPezarro <alexanderpezarro@gmail.com>
2023-03-04 15:44:39 +0000

    Add room code generation

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -8,21 +8,51 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+const activeRooms = new Set<String>();
 
-const http = require('http').Server(app);
+const server = require('http').createServer(app);
+
+function makeRoomID() {
+    while(true) {
+        let result = '';
+        // abcdefghijklmnopqrstuvwxyz
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const charactersLength = characters.length;
+        for (let index = 0; index < 5; index++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        if (!activeRooms.has(result)) {
+            activeRooms.add(result);
+            return result;
+        }
+    }
+}
 
 app.use(cors());
 
-const io = new Server();
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server + Nodemon');
   });
 
+app.get('/createRoom', (req: Request, res: Response) => {
+    res.send(makeRoomID());
+    });
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log(`${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Backend running on port ${port}`)
 })
