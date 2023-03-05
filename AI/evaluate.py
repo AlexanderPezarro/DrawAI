53ddf3814d4671c38e8b304896ed77b6ceab5f5e
Alexis Labat <alexislabat@hotmail.com>
2023-03-05 02:15:43 +0000

    Changed evaluate.py so that it continuously takes files from stdin.

--- AI/evaluate.py
+++ AI/evaluate.py
@@ -1,4 +1,4 @@
-import argparse
+import sys
 import numpy as np
 from keras import models
 import imageio.v3 as iio
@@ -8,24 +8,19 @@ from utils.utils import read_config
 
 
 if __name__ == '__main__':
-    # Create an argument parser and parse the args
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--img",
-        type=str,
-        default="",
-        help="Path where the image is located.",
-        required=True
-    )
-    flags, unparsed = parser.parse_known_args()
-
     # Extract the configuration data
     config = read_config()
     classes = config["classes"].split(",")
 
-    img = iio.imread(flags.img)[:, :, 0]
-    img = resize(img, (28, 28))
-    img = np.array([img])
+    for line in iter(sys.stdin.readline, ""):
+        if line.strip() != "":
+            line = line.replace("\n", "")
+            img = iio.imread(line)[:, :, 0]
+            img = resize(img, (28, 28))
+            img = np.array([img])
 
-    model = models.load_model("models/model_1")
-    print(classes[np.argmax(model.predict(img))])
+            model = models.load_model("models/model_1")
+            prediction = model.predict(img, verbose=0)
+
+            sys.stdout.write(f"{classes[np.argmax(prediction)]} {np.max(prediction) * 100}\n")
+            sys.stdout.flush()
