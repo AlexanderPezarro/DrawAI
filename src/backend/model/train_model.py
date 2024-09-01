10726e171037b3c449ce0df3e2b8310f43c7fb1a
alexis <alexis.labat@gamma.co.uk>
2024-09-01 21:44:13 +0400

    Added auto-download feature for absent classes. Fixed model saving

--- src/backend/model/train_model.py
+++ src/backend/model/train_model.py
@@ -1,17 +1,50 @@
 import numpy as np
 import tensorflow as tf
-
 from keras import layers, models
 from sklearn.model_selection import train_test_split
 
-from utils.utils import read_config
+from utils.utils import read_config, log
+from os import makedirs, listdir
+from os.path import exists, isfile, join
+import urllib.request as url_request
+
+DATA_DIR = "data"
+MODELS_DIR = "models"
 
 
 def create_labels(size, label):
     return np.array([label] * size, dtype=np.uint8)
 
 
+def check_missing_data(expected_datasets) -> set[int]:
+    # Ensure the dir exists
+    if not exists(DATA_DIR):
+        log("Creating missing data directory")
+        makedirs(DATA_DIR)
+
+    actual_datasets = set([
+        filename.replace(".npy", "") for filename in listdir(DATA_DIR) if isfile(join(DATA_DIR, filename))
+    ])
+
+    return set(expected_datasets).difference(actual_datasets)
+
+
+def download_data(missing_data):
+    for dataset in missing_data:
+        url = f"https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/{dataset}.npy"
+        log(f"Downloading: {url}")
+        url_request.urlretrieve(url, join(DATA_DIR, f"{dataset}.npy"))
+        log("Downloaded!")
+
+
 def load_data_sets(classes, samples_per_class) -> tuple[tuple[np.array, np.array], tuple[np.array, np.array]]:
+    # Check if all datasets are present
+    missing_data = check_missing_data(classes)
+    if len(missing_data) > 0:
+        log(f"Downloading missing datasets: {list(missing_data)}")
+        # Download any missing datasets
+        download_data(missing_data)
+
     # Load the data sets from data/ and create labels for each dataset
     input_data = np.concatenate([np.load(f"data/{name}.npy")[:samples_per_class] for name in classes])
     labels = np.concatenate([create_labels(samples_per_class, i) for i in range(len(classes))])
@@ -33,7 +66,7 @@ def load_data_sets(classes, samples_per_class) -> tuple[tuple[np.array, np.array
 
 def train_model(x_train, y_train, x_test, y_test, num_classes):
     model = models.Sequential([
-        layers.Rescaling(1./255, input_shape=(28, 28, 1)),
+        layers.Rescaling(1. / 255, input_shape=(28, 28, 1)),
         layers.Conv2D(28, (3, 3), padding='same', activation='relu'),
         layers.MaxPooling2D(),
         layers.Conv2D(56, (3, 3), padding='same', activation='relu'),
@@ -51,19 +84,27 @@ def train_model(x_train, y_train, x_test, y_test, num_classes):
     return model
 
 
+def save_model(model: models.Sequential, model_name: str) -> None:
+    # Ensure the dir exists
+    if not exists(MODELS_DIR):
+        makedirs(MODELS_DIR)
+
+    model.save(join(MODELS_DIR, model_name), zipped=False)
+
+
 if __name__ == '__main__':
     # Extract the configuration data
     config = read_config()
-    classes = config["classes"].split(",")
-    samples_per_class = int(config["samples_per_class"])
+    config_classes = config["classes"].split(",")
+    config_samples_per_class = int(config["samples_per_class"])
 
     # Gather the data sets
-    (x_train, y_train), (x_test, y_test) = load_data_sets(classes, samples_per_class)
+    (x_train, y_train), (x_test, y_test) = load_data_sets(config_classes, config_samples_per_class)
     # Train the model
-    model = train_model(x_train, y_train, x_test, y_test, len(classes))
+    model = train_model(x_train, y_train, x_test, y_test, len(config_classes))
     # Evaluate the model on the test set
     test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
     # Print the test set accuracy
     print("Accuracy: {:.2f}".format(test_acc * 100))
     # Serialise the model
-    model.save(f"models/model_1")
+    save_model(model, "model_1")
--- src/backend/model/utils/utils.py
+++ src/backend/model/utils/utils.py
@@ -1,7 +1,14 @@
 import configparser
 
+LOG_LEVEL = 2
+
 
 def read_config() -> dict[str, str]:
     config = configparser.ConfigParser()
     config.read("config.ini")
     return dict(config["config"])
+
+
+def log(text: str) -> None:
+    if LOG_LEVEL > 1:
+        print(text)
